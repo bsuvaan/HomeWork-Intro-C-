cb60c8496d1f2c1e13f3914382446e2e387378d4
Catisha <ipsuvaan@mail.ru>
2023-09-25 18:50:01 +0300

    Добавили решение для 6 задачи с гитигнор

---
+++ HomeWork_01/task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+string func(double a)
+{
+    string status = "5";
+    if (a%2 == 0) status = "yes";
+    if (a%2 != 0) status = "no";
+    return status;
+}
+
+
+Console.WriteLine("Введите ваше число: ");
+double k = Convert.ToDouble(Console.ReadLine());
+Console.Write(func(k));
