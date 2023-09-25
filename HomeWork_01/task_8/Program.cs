c235a87b32324ce6c89f751ad39869b1c4b7e0b6
Catisha <ipsuvaan@mail.ru>
2023-09-25 19:15:22 +0300

    Добавили решение для 8 задачи с гитигнор

---
+++ HomeWork_01/task_8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int func (int N)
+{
+    if (N>1)
+    {        
+        int num = 2;
+        while ((N>=num))
+        {
+         
+            Console.Write(num);
+            Console.Write(" ");
+            num += 2;               
+        }
+    }
+
+    if (N<=1)
+    {
+        Console.WriteLine("Таких значений в натуральных числах нет");
+    }
+    
+    return 0;
+}
+
+int k = Int32.Parse(Console.ReadLine());
+func(k);
