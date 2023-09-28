3c324d61000483e78aa9365f300e65bac751f7d1
Catisha <ipsuvaan@mail.ru>
2023-09-28 17:56:56 +0300

    add 'Решили первую задачу'

--- lesson2/practical/task1/Program.cs
+++ lesson2/practical/task1/Program.cs
@@ -2,4 +2,37 @@
 //маленькие буквы “к” заменить большими “К”, а большие “С” заменить
 //маленькими “с”
 
-string Replase()
+string Replase(string origin_text, char Changed_Symbol, char Changing_Symbol)
+{
+    string Result = String.Empty;
+    int l = origin_text.Length; 
+    for (int i=1; i < l; i++)
+    {
+        if (origin_text[i] == Changed_Symbol) Result = Result + $"{Changing_Symbol}";
+        else Result = Result + $"{origin_text[i]}";
+    }
+
+    return Result;
+}
+
+void Write(string outp)
+{
+    Console.WriteLine(outp);
+    Console.WriteLine();
+}
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, " +
+            "ежели бы вас послали вместо нашего милого " + 
+            "Винценгероде, вы бы взяли приступом согласие " +
+            "прусского короля. Вы так красноречивы. Вы " +            
+            "дадите мне чаю?";
+
+Write(text);
+string new_text = Replase(text, ' ', '|');
+Write(new_text);
+new_text = Replase(new_text, 'к', 'К');
+Write(new_text);
+new_text = Replase(new_text, 'с', 'С');
+Write(new_text);
+
+
