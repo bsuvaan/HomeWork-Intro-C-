8fb30ab4ac4a4086c91b99296156ce051180bf0c
Catisha <ipsuvaan@mail.ru>
2023-09-28 18:32:13 +0300

    Решили 15 задачу

---
+++ lesson2/homework/task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+string input_text = Console.ReadLine();
+if (input_text.Length >= 3)
+{
+    Console.WriteLine("Третье в записи число: " + input_text[2]);
+}
+else Console.WriteLine("У введенного числа нет третьей цифры");
