91d976c2a6fd0341c528aeab8aeb5beed3540cf9
Catisha <ipsuvaan@mail.ru>
2023-09-28 19:42:10 +0300

    Решили 15 задачу

---
+++ lesson2/homework/task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool Is_It_Day_Out (char day)
+{
+    bool flag = false;
+    string weekend = "67";
+    for (int i=0; i<weekend.Length; i++)
+    {
+        if (day == weekend[i]) 
+        {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+
+
+Console.Write("Введите номер дня: ");
+string Day = Console.ReadLine();
+if (Is_It_Day_Out(Day[0])) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
+
