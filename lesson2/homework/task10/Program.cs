8905dc35bc4275081536d73835fd23e201a70eb6
Catisha <ipsuvaan@mail.ru>
2023-09-28 18:26:01 +0300

    Решили 2 задачу

---
+++ lesson2/homework/task10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string input_text = Console.ReadLine();
+if (input_text.Length == 3)
+{
+    Console.WriteLine("Второе в записи число: " + input_text[2]);
+}
+else Console.WriteLine("Число должно быть трехзначным");
